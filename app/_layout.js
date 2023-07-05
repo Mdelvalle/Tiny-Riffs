693222586820b2734a8956844e54f8448caaee9c
Mdelvalle <migdelv@gmail.com>
2023-07-04 22:56:54 -0400

    Add expo-av and react-native-track-player

--- app/_layout.js
+++ app/_layout.js
@@ -1,12 +1,26 @@
 import { FONT } from '@constants/theme';
 import { useFonts } from 'expo-font';
-import { Slot, SplashScreen } from 'expo-router';
+import { Slot } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 const AppLayout = () => {
   const [fontsLoaded] = useFonts(FONT.asset);
 
+  useEffect(() => {
+    if (fontsLoaded) {
+      // Hide the splash screen after the fonts have loaded and the
+      // UI is ready.
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  // Prevent rendering until the font has loaded
   if (!fontsLoaded) {
-    return <SplashScreen />;
+    return null;
   }
 
   return <Slot />;
