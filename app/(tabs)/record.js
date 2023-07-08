39da7e96e99f727e0a22933290d62951c7bbbff3
Mdelvalle <migdelv@gmail.com>
2023-07-08 17:22:14 -0400

    Record audio and play back after recording ends

--- app/(tabs)/record.js
+++ app/(tabs)/record.js
@@ -2,23 +2,100 @@ import RecordButton from '@components/record/RecordButton';
 import TimeSignatureButton from '@components/record/TimeSignatureButton';
 import TimesToLoop from '@components/record/TimesToLoop';
 import { COLOR, SIZE } from '@constants/theme';
+import { Audio } from 'expo-av';
 import Constants from 'expo-constants';
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-const Record = () => {
+function Record() {
   const [selectedButton, setSelectedButton] = useState('');
-  const [recording, setRecording] = useState(false);
+  const [recording, setRecording] = useState(null);
+  const [recordingStatus, setRecordingStatus] = useState(false);
+
+  useEffect(() => {
+    console.log(`> Recording has ${recording ? 'started' : 'ended'}.`);
+  }, [recording]);
+
+  useEffect(() => {
+    async function getPermission() {
+      try {
+        const permission = await Audio.requestPermissionsAsync();
+        console.log('Permission Granted', permission.granted);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getPermission();
+
+    return () => {
+      if (recording) {
+        stopRecording();
+      }
+    };
+  }, []);
 
   const handleTimeSignatureButtonPress = (button) => {
     setSelectedButton(button);
   };
 
-  const handleRecordButtonPress = () => {
-    setRecording(!recording);
-  };
+  async function startRecording() {
+    try {
+      const newRecording = new Audio.Recording();
+      console.log('Starting recording..');
+      await newRecording.prepareToRecordAsync(
+        Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY,
+      );
+      await newRecording.startAsync();
+      setRecording(newRecording);
+      setRecordingStatus('recording');
+    } catch (err) {
+      console.error('Failed to START recording', err);
+    }
+  }
+
+  async function stopRecording() {
+    try {
+      if (recordingStatus === 'recording') {
+        console.log('Stopping recording..');
+        await recording.stopAndUnloadAsync();
+        const recordingUri = recording.getURI();
+
+        // Name the new recording file
+        // const fileName = `tR-${Date.now()}.mp3`;
+
+        // Save the file
+
+        // Play the file
+        const playbackObject = new Audio.Sound();
+        await playbackObject.loadAsync({
+          uri: recordingUri,
+        });
+        await playbackObject.playAsync();
+
+        // Update recording state
+        setRecording(null);
+        setRecordingStatus('stopped');
+
+        return recordingUri;
+      }
+    } catch (error) {
+      console.error('Failed to STOP recording', error);
+    }
+  }
+
+  async function handleRecordButtonPress() {
+    if (recording) {
+      const audioUri = await stopRecording();
+      if (audioUri) {
+        console.log('Saved the file to', audioUri);
+      }
+    } else {
+      await startRecording();
+    }
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -57,12 +134,12 @@ const Record = () => {
       </View>
       <TimesToLoop />
       <RecordButton
-        recording={recording}
+        recording={Boolean(recording)}
         onPress={() => handleRecordButtonPress()}
       />
     </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
