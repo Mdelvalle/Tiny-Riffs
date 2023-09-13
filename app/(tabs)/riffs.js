4b348b741e777296e6b6f282da42053ed7a1af7a
Mdelvalle <migdelv@gmail.com>
2023-09-13 00:25:32 -0400

    Omit .trashed files & Play/Pause Tracks

--- app/(tabs)/riffs.js
+++ app/(tabs)/riffs.js
@@ -3,17 +3,19 @@ import { SetupService } from '@services';
 import { StorageAccessFramework as SAF } from 'expo-file-system';
 import { useEffect, useState } from 'react';
 import {
+  ActivityIndicator,
   FlatList,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
 } from 'react-native';
+import TrackPlayer, { State } from 'react-native-track-player';
 
 const Riffs = () => {
   const [riffs, setRiffs] = useState([]);
   const [directoryUri, setDirectoryUri] = useState(null);
-  // const [isPlayerReady, setIsPlayerReady] = useState(false);
+  const [isPlayerReady, setIsPlayerReady] = useState(false);
 
   // Get storage permissions
   useEffect(() => {
@@ -34,15 +36,15 @@ const Riffs = () => {
     getStoragePermission();
   }, []);
 
+  // Start TrackPlayer setup service
   useEffect(() => {
     const setup = async () => {
-      await SetupService();
-
-      // if (isSetup) {
-      //   await addTrack()
-      // }
-
-      // setIsPlayerReady(() => isSetup);
+      try {
+        await SetupService();
+        setIsPlayerReady(() => true);
+      } catch (error) {
+        console.log('Error setting up TrackPlayer service', error);
+      }
     };
 
     setup();
@@ -55,21 +57,24 @@ const Riffs = () => {
         if (!directoryUri) return;
 
         const assets = await SAF.readDirectoryAsync(directoryUri);
-        const riffData = assets.map((file, index) => {
-          const decodedFileUri = decodeURIComponent(file);
-          const fileName = decodedFileUri
-            .split('/')
-            .pop()
-            .split('.')
-            .slice(0, -1)
-            .join('.');
+        const riffData = assets
+          // Don't include files in the trash
+          .filter((file) => !file.includes('.trashed'))
+          .map((file, index) => {
+            const decodedFileUri = decodeURIComponent(file);
+            const fileName = decodedFileUri
+              .split('/')
+              .pop()
+              .split('.')
+              .slice(0, -1)
+              .join('.');
 
-          return {
-            id: index.toString(),
-            url: file,
-            fileName,
-          };
-        });
+            return {
+              id: `${fileName}-${index.toString()}`,
+              url: file,
+              fileName,
+            };
+          });
 
         setRiffs(riffData);
       } catch (error) {
@@ -80,8 +85,31 @@ const Riffs = () => {
     fetchRiffs();
   }, [directoryUri]);
 
+  const handleTrackPlayback = async ({ id, url, fileName }) => {
+    try {
+      const currentState = await TrackPlayer.getState();
+
+      if (currentState === State.Playing) {
+        await TrackPlayer.pause();
+      } else {
+        await TrackPlayer.reset();
+        await TrackPlayer.add({
+          id,
+          url,
+          title: fileName,
+          artist: '',
+        });
+        await TrackPlayer.play();
+      }
+    } catch (error) {
+      console.log('Error playing track:', error);
+    }
+  };
+
   const renderItem = ({ item, index }) => (
-    <TouchableOpacity style={[styles.listItem, index > 0 && styles.mt]}>
+    <TouchableOpacity
+      style={[styles.listItem, index > 0 && styles.mt]}
+      onPress={() => handleTrackPlayback(item)}>
       <Text style={[styles.fileMeta, styles.fileName]}>{item.fileName}</Text>
       <View style={styles.metaContainer}>
         <Text style={styles.fileMeta}>4/4</Text>
@@ -91,9 +119,10 @@ const Riffs = () => {
     </TouchableOpacity>
   );
 
-  // if (!isPlayerReady) {
-  //   return <ActivityIndicator />;
-  // }
+  // Loading animation
+  if (!directoryUri || !isPlayerReady || !riffs) {
+    return <ActivityIndicator />;
+  }
 
   return (
     <View style={styles.container}>
